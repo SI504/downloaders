f3145f4c18065025bbaba9b6832f93599a14f6a2
Michael <mlhess@umich.edu>
2021-01-03 12:59:14 -0500

    make this quiet

--- week2.sh
+++ week2.sh
@@ -7,8 +7,9 @@ cd
 rm -rf week2
 mkdir week2
 cd week2
-git clone https://github.com/SI504/company.git
+git clone -q  https://github.com/SI504/company.git
 rm -rf company/.git
-git clone https://github.com/SI504/world.git
+echo "Half way done"
+git clone -q https://github.com/SI504/world.git
 rm -rf world/.git
 echo "Done"
