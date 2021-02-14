260cf4f87a29e84015030571048c1fde90126fb5
Michael Hess <mlhess@umich.edu>
2021-02-14 15:03:30 -0500

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,10 +1,11 @@
 import time
-lower = 1
-upper = 1000000
+import sys
+lower = sys.argv[1]
+upper = sys.argv[2]
 
-print("Prime numbers between", lower, "and", upper, "are:")
+print("Prime numbers between", str(lower), "and", str(upper), "are:")
 
-for num in range(lower, upper + 1):
+for num in range(int(lower), int(upper) + 1):
    # all prime numbers are greater than 1
    if num > 1:
        for i in range(2, num):
