94c5449a4399f2c2c5d4833ff72a937627eb930e
Michael Hess <mlhess@umich.edu>
2021-02-18 15:00:58 -0500

    Update prime.sh

--- prime.sh
+++ prime.sh
@@ -1,5 +1,5 @@
-python prime.py 1 100 &
-python prime.py 300 300000000 &
-python prime.py 300 30000 &
-python prime.py 300 30000 &
-python prime.py 300 300000 &
+python3 prime.py 1 100 &
+python3 prime.py 300 300000000 &
+python3 prime.py 300 30000 &
+python3 prime.py 300 30000 &
+python3 prime.py 300 300000 &
