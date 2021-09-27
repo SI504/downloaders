a0526d3a0dff68a3e396a705df67cca6c76278ff
Michael Hess <mlhess@umich.edu>
2021-09-27 11:02:24 -0400

    Update prime.sh

--- prime.sh
+++ prime.sh
@@ -8,4 +8,13 @@ python3 prime.py 300 30000 &
 python3 prime.py 300 300000 &
 seq 3 | xargs -P0 -n1 timeout 5 md5sum /dev/zero & 
 seq 50 | xargs -P0 -n1 timeout 5 md5sum /dev/zero & 
-
+python3 prime.py 1 100 &
+python3 prime.py 300 300000000 &
+python3 prime.py 300 30000 &
+python3 prime.py 300 30000 &
+python3 prime.py 300 300000 &
+python3 prime.py 300 30000 &
+python3 prime.py 300 30000 &
+python3 prime.py 300 300000 &
+seq 3 | xargs -P0 -n1 timeout 5 md5sum /dev/zero & 
+seq 50 | xargs -P0 -n1 timeout 5 md5sum /dev/zero & 
